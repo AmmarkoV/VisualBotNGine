5cfb42c0dacd80ac09223c97d684db48032a9b0b
Ammar Qammaz <ammarkov@gmail.com>
2016-12-21 23:28:28 +0200

    ...

--- Engines/Dota2/online/getStats.sh
+++ Engines/Dota2/online/getStats.sh
@@ -2,10 +2,9 @@
 
 rm winning
 wget https://www.dotabuff.com/heroes/winning  
-sed -e 's/<[^>]*>//g' winning > winclean
-cat winclean | grep -o "HeroWin RatePick RateKDA Ratio.*" > winclean2
-sed -i 's/"HeroWin RatePick RateKDA Ratio"/ /g' winclean2
-sed -i 's/%/ /g' winclean2
-sed -i 's/^[a-z][a-z]*/& /;s/[0-9][0-9]*/&  /' winclean2 
+sed -e 's/<[^>]*>/ /g' winning > winclean
+cat winclean | grep -o "Hero  Win Rate  Pick Rate  KDA Ratio.*" > winclean2
+sed -i 's/"Hero  Win Rate  Pick Rate  KDA Ratio"/ /g' winclean2
+sed -i 's/%/ /g' winclean2 
 
 exit 0
